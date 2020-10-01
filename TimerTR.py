1db6d9a9bf84ae6a5abc1c8eb6991e5a341e1d6e
dioXen33 <70483599+dioXen33@users.noreply.github.com>
2020-10-01 20:38:49 +0200

    Update TimerTR.py

--- TimerTR.py
+++ TimerTR.py
@@ -34,7 +34,7 @@ async def startTR(dic, channel, guild):
     for i in dic:
         content1 += str(i) + " -=> " + str(int((dic[i] - datetime.datetime.utcnow()).seconds / 60)) + "m" + str(int((dic[i] - datetime.datetime.utcnow()).seconds % 60)) + "s" + "\n"
     id = await channel.send("```" + content1 + "```")
-    while True:
+    while True :
         content1 = ""
         needpop = 0
         await asyncio.sleep(1)
@@ -65,4 +65,4 @@ async def on_message(message):
             dic[stuff[0][-2:]] = message.created_at + datetime.timedelta(seconds=findtime(stuff[1]) - 1)
         await startTR(dic, message.channel, message.guild)
         return 0
-    client.run(NzU5ODA3NTUzOTY5MzI0MTEz.X3C37Q.yjXUucbZn5wQRkZ1
+    client.run(NzU5ODA3NTUzOTY5MzI0MTEz.X3C37Q.yjXUucbZn5wQRkZ1-2VT9XmXITs)
